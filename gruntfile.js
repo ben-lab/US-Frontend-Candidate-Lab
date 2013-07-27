23a5dea3008fbbae1d949d5fc460f8d1f124c506
Benjamin Smith <benjamin.smith@72andsunny.com>
2013-07-27 12:04:39 -0700

    implementing grunt for better workflow

---
+++ gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['js/**/*.js'],
+        dest: 'www/js/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'www/js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    jshint: {
+      files: ['gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+   compass: {
+      dist: {
+        options: {
+          sassDir: 'scss',
+          cssDir: 'www/css'
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      css: {
+        files: ['scss/*.scss'],
+        tasks: ['compass:dist']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+  grunt.registerTask('test', ['jshint']);
+
+  grunt.registerTask('default', ['jshint', 'compass', 'concat', 'uglify']);
+
+};
