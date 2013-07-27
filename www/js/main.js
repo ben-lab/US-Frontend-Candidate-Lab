5e10149ba25228f5330fe097bbd1696a6331ab92
Benjamin Smith <benjamin.smith@72andsunny.com>
2013-07-27 15:14:05 -0700

    starting media queries

--- www/js/main.js
+++ www/js/main.js
@@ -12,7 +12,7 @@
 		if( $(this).hasClass('yes')) {
 			inviteResponse.html("Awesome! Be sure to add this event to your <a href='https://www.google.com/calendar/render?action=TEMPLATE&text=MDC+Partners+Polaroid+Celebration&dates=20131031T010000Z/20131031T030000Z&details=Book+Signing+%26+Cocktail+Hour+of+Instant+%E2%80%94+The+Story+of+Polaroid+by+author+Christopher+Bonanos.&location=The+MDC+Innovation+Centre,+745+Fifth+Avenue,+19th+Floor&trp=false&sprop&sf=true&output=xml' target='_blank'>Google Calendar.</a> ");
 		} else {
-			inviteResponse.html("Awww, you sure you don't want to come?");
+			inviteResponse.html("Awww! You sure you don't want to come?");
 		}
 		showResponse();
 	});
